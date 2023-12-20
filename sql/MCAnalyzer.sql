e40da7bd873a84fa4503be5ad3d85541f4d7f4cc
VladiusVostokus <kovalenkov222@gmail.com>
2023-12-20 18:56:33 +0200

    Update sql script and .env

--- sql/MCAnalyzer.sql
+++ sql/MCAnalyzer.sql
@@ -8,22 +8,22 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema lab4
+-- Schema mcanalyzer
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `lab4` ;
+DROP SCHEMA IF EXISTS `mcanalyzer` ;
 
 -- -----------------------------------------------------
--- Schema lab4
+-- Schema mcanalyzer
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `lab4` DEFAULT CHARACTER SET utf8mb3 ;
-USE `lab4` ;
+CREATE SCHEMA IF NOT EXISTS `mcanalyzer` DEFAULT CHARACTER SET utf8mb3 ;
+USE `mcanalyzer` ;
 
 -- -----------------------------------------------------
--- Table `lab4`.`Role`
+-- Table `mcanalyzer`.`Role`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `lab4`.`Role` ;
+DROP TABLE IF EXISTS `mcanalyzer`.`Role` ;
 
-CREATE TABLE IF NOT EXISTS `lab4`.`Role` (
+CREATE TABLE IF NOT EXISTS `mcanalyzer`.`Role` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
@@ -33,11 +33,11 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `lab4`.`User`
+-- Table `mcanalyzer`.`User`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `lab4`.`User` ;
+DROP TABLE IF EXISTS `mcanalyzer`.`User` ;
 
-CREATE TABLE IF NOT EXISTS `lab4`.`User` (
+CREATE TABLE IF NOT EXISTS `mcanalyzer`.`User` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `password` VARCHAR(255) NOT NULL,
   `nickname` VARCHAR(255) NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `lab4`.`User` (
   INDEX `fk_User_Role1_idx` (`Role_id` ASC) VISIBLE,
   CONSTRAINT `fk_User_Role1`
     FOREIGN KEY (`Role_id`)
-    REFERENCES `lab4`.`Role` (`id`)
+    REFERENCES `mcanalyzer`.`Role` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -55,11 +55,11 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `lab4`.`MentionReport`
+-- Table `mcanalyzer`.`MentionReport`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `lab4`.`MentionReport` ;
+DROP TABLE IF EXISTS `mcanalyzer`.`MentionReport` ;
 
-CREATE TABLE IF NOT EXISTS `lab4`.`MentionReport` (
+CREATE TABLE IF NOT EXISTS `mcanalyzer`.`MentionReport` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(255) NOT NULL,
   `creationDate` DATETIME NOT NULL,
@@ -69,17 +69,17 @@ CREATE TABLE IF NOT EXISTS `lab4`.`MentionReport` (
   INDEX `fk_MentionReport_User1_idx` (`User_id` ASC) VISIBLE,
   CONSTRAINT `fk_MentionReport_User1`
     FOREIGN KEY (`User_id`)
-    REFERENCES `lab4`.`User` (`id`))
+    REFERENCES `mcanalyzer`.`User` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `lab4`.`PubRequest`
+-- Table `mcanalyzer`.`PubRequest`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `lab4`.`PubRequest` ;
+DROP TABLE IF EXISTS `mcanalyzer`.`PubRequest` ;
 
-CREATE TABLE IF NOT EXISTS `lab4`.`PubRequest` (
+CREATE TABLE IF NOT EXISTS `mcanalyzer`.`PubRequest` (
   `User_id` INT UNSIGNED NOT NULL,
   `date` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
@@ -87,17 +87,17 @@ CREATE TABLE IF NOT EXISTS `lab4`.`PubRequest` (
   INDEX `fk_PubRequest_User1_idx` (`User_id` ASC) VISIBLE,
   CONSTRAINT `fk_PubRequest_User1`
     FOREIGN KEY (`User_id`)
-    REFERENCES `lab4`.`User` (`id`))
+    REFERENCES `mcanalyzer`.`User` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `lab4`.`PubReview`
+-- Table `mcanalyzer`.`PubReview`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `lab4`.`PubReview` ;
+DROP TABLE IF EXISTS `mcanalyzer`.`PubReview` ;
 
-CREATE TABLE IF NOT EXISTS `lab4`.`PubReview` (
+CREATE TABLE IF NOT EXISTS `mcanalyzer`.`PubReview` (
   `status` VARCHAR(45) NOT NULL,
   `User_id` INT UNSIGNED NOT NULL,
   `PubRequest_User_id` INT UNSIGNED NOT NULL,
@@ -105,10 +105,10 @@ CREATE TABLE IF NOT EXISTS `lab4`.`PubReview` (
   INDEX `fk_PubReview_PubRequest1_idx` (`PubRequest_User_id` ASC) VISIBLE,
   CONSTRAINT `fk_PubReview_User1`
     FOREIGN KEY (`User_id`)
-    REFERENCES `lab4`.`User` (`id`),
+    REFERENCES `mcanalyzer`.`User` (`id`),
   CONSTRAINT `fk_PubReview_PubRequest1`
     FOREIGN KEY (`PubRequest_User_id`)
-    REFERENCES `lab4`.`PubRequest` (`User_id`)
+    REFERENCES `mcanalyzer`.`PubRequest` (`User_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -116,11 +116,11 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `lab4`.`ResultData`
+-- Table `mcanalyzer`.`ResultData`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `lab4`.`ResultData` ;
+DROP TABLE IF EXISTS `mcanalyzer`.`ResultData` ;
 
-CREATE TABLE IF NOT EXISTS `lab4`.`ResultData` (
+CREATE TABLE IF NOT EXISTS `mcanalyzer`.`ResultData` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `sourse` VARCHAR(255) NOT NULL,
   `body` VARCHAR(255) NOT NULL,
@@ -132,7 +132,7 @@ CREATE TABLE IF NOT EXISTS `lab4`.`ResultData` (
   INDEX `fk_ResultData_MentionReport1_idx` (`MentionReport_id` ASC, `MentionReport_User_id` ASC) VISIBLE,
   CONSTRAINT `fk_ResultData_MentionReport1`
     FOREIGN KEY (`MentionReport_id` , `MentionReport_User_id`)
-    REFERENCES `lab4`.`MentionReport` (`id` , `User_id`))
+    REFERENCES `mcanalyzer`.`MentionReport` (`id` , `User_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
@@ -142,12 +142,12 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `lab4`.`Role`
+-- Data for table `mcanalyzer`.`Role`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `lab4`;
-INSERT INTO `lab4`.`Role` (`id`, `name`, `description`) VALUES (1, 'User', 'Registered user, can get info about mentions');
-INSERT INTO `lab4`.`Role` (`id`, `name`, `description`) VALUES (2, 'Editor', 'Can do the same as the user, can edit data');
-INSERT INTO `lab4`.`Role` (`id`, `name`, `description`) VALUES (3, 'Admin', 'Can do the same as other, can give roles and can block users.');
+USE `mcanalyzer`;
+INSERT INTO `mcanalyzer`.`Role` (`id`, `name`, `description`) VALUES (1, 'User', 'Registered user, can get info about mentions');
+INSERT INTO `mcanalyzer`.`Role` (`id`, `name`, `description`) VALUES (2, 'Editor', 'Can do the same as the user, can edit data');
+INSERT INTO `mcanalyzer`.`Role` (`id`, `name`, `description`) VALUES (3, 'Admin', 'Can do the same as other, can give roles and can block users.');
 
 COMMIT;
