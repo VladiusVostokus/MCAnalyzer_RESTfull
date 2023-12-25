4c562e7ef6c7f4891ba6d85049de73da1e1bfab1
VladiusVostokus <kovalenkov222@gmail.com>
2023-12-25 16:13:34 +0200

    Handle a server error

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/userRoutes', userRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req ,res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
