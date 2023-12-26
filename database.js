1c11aa352f02e66f45caff086c6f572fae69d735
VladiusVostokus <kovalenkov222@gmail.com>
2023-12-26 16:40:29 +0200

    function to get all roles

--- database.js
+++ database.js
@@ -4,15 +4,22 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const access = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-  };
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+};
 
 const pool = mysql.createPool(access).promise();
 
+const getRoles = async() => {
+  const [result] = await pool.query(`SELECT * FROM mcanalyzer.role;`);
+  return result;
+};
+
 (async () =>{
-    const result = await pool.query(`SELECT * FROM mcanalyzer.role;`);
-    console.log(result);
+  console.log(await getRoles());
 })();
+
+
+module.exports = pool;
