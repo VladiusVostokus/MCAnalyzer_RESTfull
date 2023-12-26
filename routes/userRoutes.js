eef1894e32de9d1a3fc801ed28a1ef7b44f38dce
VladiusVostokus <kovalenkov222@gmail.com>
2023-12-26 17:54:03 +0200

    shorter routers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,16 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const router = express.Router();
 
-router.get('/', userController.Get);
-router.post('/',userController.Create);
-router.get('/:userId', userController.getUser);
-router.post('/:userId', userController.createUser);
-router.patch('/:userId', userController.updateUser);
-router.delete('/:userId', userController.deleteUser);
+router
+    .route('/')
+    .get(userController.Get)
+    .post(userController.Create)
+
+router
+    .route('/:userId')
+    .get(userController.getUser)
+    .post(userController.createUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
 
 module.exports = router;
